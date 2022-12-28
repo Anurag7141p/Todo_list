47cdf01be31c7826bee4779da1a5f385ff1ca771
anurag <anuragnbr@gmail.com>
2022-12-28 17:21:18 +0530

    Todo_list

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+  }
+  task:any;
+  id:any;
+  list:any=[];
+
+  addTask(task:any)
+  {
+    if(task==null)
+    {
+      alert('please enter a value')
+    }
+    else
+    {
+      this.list.push({id:this.list.length,name:task})
+    }
+  }
+
+  remove(id:any){
+    console.log(id);
+    this.list=this.list.filter((item:any)=>item.id!==id)
+    
+  }
+}
